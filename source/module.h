851af7fe822177339f465f0065ec56a5aa9e8bb2
mcuking <mcuking.tang@gmail.com>
2021-07-19 10:13:41 +0800

    feat: 添加 解析 Wasm 二进制文件自定义段 功能

--- source/module.h
+++ source/module.h
@@ -6,6 +6,22 @@
 
 #include <stdlib.h>
 
+// 段 ID 的枚举
+typedef enum {
+    CustomID,// 自定义段 ID
+    TypeID,  //类型段 ID
+    ImportID,// 导入段 ID
+    FuncID,  // 函数段 ID
+    TableID, // 表段 ID
+    MemID,   // 内存段 ID
+    GlobalID,// 全局段 ID
+    ExportID,// 导出段 ID
+    StartID, // 起始段 ID
+    ElemID,  // 元素段 ID
+    CodeID,  // 代码段 ID
+    DataID   //数据段 ID
+} SecID;
+
 // Wasm 内存格式对应的结构体
 typedef struct Module {
     const uint8_t *bytes;// 用于存储 Wasm 二进制模块的内容
