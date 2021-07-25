34e52b30d37f8e5847234908edbe0e6af574ab30
mcuking <mcuking.tang@gmail.com>
2021-07-25 20:12:51 +0800

    fix: 修复 获取控制块类型函数 get_block_type 返回类型错误问题

--- source/utils.c
+++ source/utils.c
@@ -153,7 +153,8 @@ uint64_t get_type_mask(Type *type) {
     return mask;
 }
 
-// 根据目前版本的 Wasm 标准，控制块只能有一个返回值
+// 根据目前版本的 Wasm 标准，控制块不能有参数，且最多只能只能有一个返回值
+// 注：目前多返回值提案还没有进入 Wasm 标准
 uint32_t block_type_results[4][1] = {{I32}, {I64}, {F32}, {F64}};
 
 Type block_types[5] = {
@@ -177,10 +178,13 @@ Type block_types[5] = {
                 .results = block_type_results[3],
         }};
 
-// 根据表示该控制块的类型的值（占一个字节），返回控制块的类型（或签名），即控制块的入参/出参的数量和类型
+// 根据表示该控制块的类型的值（占一个字节），返回控制块的类型（或签名），即控制块的返回值的数量和类型
+// 0x7f 表示有一个 i32 类型返回值、0x7e 表示有一个 i64 类型返回值、0x7d 表示有一个 f32 类型返回值、0x7c 表示有一个 f64 类型返回值、0x40 表示没有返回值
+// 注：目前多返回值提案还没有进入 Wasm 标准，根据当前版本的 Wasm 标准，控制块不能有参数，且最多只能只能有一个返回值
 Type *get_block_type(uint8_t value_type) {
     switch (value_type) {
-        case BLOCK:
+        case 0x40:
+            return &block_types[0];
         case I32:
             return &block_types[1];
         case I64:
